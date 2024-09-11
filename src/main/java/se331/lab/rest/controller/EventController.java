1d7894052a4d75d72c1c53495e3a3a30a0cdda0c
Sorod Yawa <sorod_yawa@cmu.ac.th>
2024-09-12 01:08:16 +0700

    3.1* update getEventLists to receive parameter

--- src/main/java/se331/lab/rest/controller/EventController.java
+++ src/main/java/se331/lab/rest/controller/EventController.java
@@ -89,7 +89,15 @@ public class EventController {
                 .build());
     }
     @GetMapping("events")
-    public ResponseEntity<?> getEventLists() {
-        return ResponseEntity.ok(eventList);
+    public ResponseEntity<?> getEventLists(@RequestParam(value = "_limit", required = false) Integer perPage,
+                                           @RequestParam(value = "_page", required = false) Integer page) {
+        perPage = perPage == null ? eventList.size() : perPage;
+        page = page == null ? 1 : page;
+        Integer firstIndex = (page - 1) * perPage;
+        List<Event> output = new ArrayList<>();
+        for (int i = firstIndex; i < firstIndex + perPage; i++) {
+            output.add(eventList.get(i));
+        }
+        return ResponseEntity.ok(output);
     }
 }
