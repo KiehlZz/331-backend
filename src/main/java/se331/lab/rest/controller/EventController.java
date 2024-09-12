4e25b4371d49414cc0bb6bf44e2ab963115569ec
Sorod Yawa <sorod_yawa@cmu.ac.th>
2024-09-13 01:15:55 +0700

    4.6* Add next and prev button

--- src/main/java/se331/lab/rest/controller/EventController.java
+++ src/main/java/se331/lab/rest/controller/EventController.java
@@ -1,6 +1,7 @@
 package se331.lab.rest.controller;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -102,13 +103,15 @@ public class EventController {
         page = page == null ? 1 : page;
         Integer firstIndex = (page - 1) * perPage;
         List<Event> output = new ArrayList<>();
+        HttpHeaders responseHeader = new HttpHeaders();
+        responseHeader.set("x-total-count",String.valueOf(eventList.size()));
         try {
             for (int i = firstIndex; i < firstIndex + perPage; i++) {
                 output.add(eventList.get(i));
             }
-            return ResponseEntity.ok(output);
+            return new ResponseEntity<>(output,responseHeader,HttpStatus.OK);
         }catch (IndexOutOfBoundsException ex ) {
-            return ResponseEntity.ok(output);
+            return new ResponseEntity<>(output,responseHeader,HttpStatus.OK);
         }
     }
 
@@ -134,7 +137,8 @@ public class EventController {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173");
+                        .allowedOrigins("http://localhost:5173")
+                        .exposedHeaders("x-total-count");
             }
         };
     }
